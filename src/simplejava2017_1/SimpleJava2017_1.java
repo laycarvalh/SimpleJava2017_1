85d1e4706c7bcd302368c1be75becccb852a84fa
Lay Carvalho <lay.carvalh@gmail.com>
2017-06-04 23:56:21 -0300

    Linhas IDE Funcional

--- src/simplejava2017_1/SimpleJava2017_1.java
+++ src/simplejava2017_1/SimpleJava2017_1.java
@@ -15,8 +15,8 @@ public class SimpleJava2017_1 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+//    public static void main(String[] args) {
+//        // TODO code application logic here
+//    }
     
 }
--- src/testes/IDE.java
+++ src/testes/IDE.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 
-package simplejava2017_1;
+package testes;
 
 import excecoes.SyntaxError;
 import gramatica.ErroSintaticoListener;
@@ -28,6 +28,7 @@ public class IDE extends javax.swing.JFrame {
      */
     public IDE() {
         initComponents();
+        
     }
 
     /**
@@ -45,9 +46,6 @@ public class IDE extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jScrollPane2 = new javax.swing.JScrollPane();
         textSaidaErro = new javax.swing.JTextArea();
-        jMenuBar1 = new javax.swing.JMenuBar();
-        jMenu1 = new javax.swing.JMenu();
-        jMenu2 = new javax.swing.JMenu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -61,6 +59,8 @@ public class IDE extends javax.swing.JFrame {
         textEditor.setColumns(20);
         textEditor.setRows(5);
         jScrollPane1.setViewportView(textEditor);
+        TextLineNumbers contadorLinhas = new TextLineNumbers(textEditor);
+        jScrollPane1.setRowHeaderView(contadorLinhas);
 
         jLabel1.setText("Saída");
 
@@ -68,42 +68,31 @@ public class IDE extends javax.swing.JFrame {
         textSaidaErro.setRows(5);
         jScrollPane2.setViewportView(textSaidaErro);
 
-        jMenu1.setText("File");
-        jMenuBar1.add(jMenu1);
-
-        jMenu2.setText("Edit");
-        jMenuBar1.add(jMenu2);
-
-        setJMenuBar(jMenuBar1);
-
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jScrollPane1)
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(169, 169, 169)
-                        .addComponent(btnCompilar))
-                    .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jLabel1)))
-                .addContainerGap(180, Short.MAX_VALUE))
+            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 400, Short.MAX_VALUE)
             .addComponent(jScrollPane2)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel1)
+                    .addComponent(btnCompilar))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
+                .addGap(16, 16, 16)
                 .addComponent(btnCompilar)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 132, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jLabel1)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(32, Short.MAX_VALUE))
         );
 
         pack();
@@ -120,12 +109,13 @@ public class IDE extends javax.swing.JFrame {
 	parser.addErrorListener(errorCollector);
 	System.out.println(errorCollector.getErrors().size());
         
-        ParseTree	tree = parser.prog();
+        ParseTree  tree = parser.prog();
 				
 	SimpleJavaVisitor loader = new SimpleJavaVisitor();
 	loader.visit(tree);
 	
         if(errorCollector.getErrors().size() > 0){
+            textSaidaErro.append("Atenção: ERRO\n");
             for (int i = 0 ;i < errorCollector.getErrors().size() ; i++) {
 		SyntaxError e =  errorCollector.getErrors().get(i);
 		// RecognitionExceptionUtil is my custom class discussed next.
@@ -183,9 +173,6 @@ public class IDE extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JToggleButton btnCompilar;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JMenu jMenu1;
-    private javax.swing.JMenu jMenu2;
-    private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JTextArea textEditor;
